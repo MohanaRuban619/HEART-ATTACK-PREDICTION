724eed041ea5bb171351927cb3555c3a2af593eb
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-07 21:25:52 +0530

    Add files via upload

---
+++ HEART_ATTACK_PROJECT/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for HEART_ATTACK_PREDICTION project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HEART_ATTACK_PROJECT.settings')
+
+application = get_wsgi_application()
