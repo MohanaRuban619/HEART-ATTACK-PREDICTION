15f7b7d317f2a295e22c34b6ed6e0ae4c98f5ae7
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-07 21:42:09 +0530

    Update settings.py

--- HEART_ATTACK_PROJECT/settings.py
+++ HEART_ATTACK_PROJECT/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@z3i3xv$s4ppt9@&9gvt4gcfi9*#=wrx%u85ump7b2d6vjl1ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [heartattackprediction1.herokuapp.com]
 
 
 # Application definition
