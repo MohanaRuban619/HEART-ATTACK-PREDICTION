cdacdf037b68d7a687bc27bb5b5132a483a6cc56
Mohana Ruban <96573480+MohanaRuban619@users.noreply.github.com>
2022-01-07 21:24:25 +0530

    Add files via upload

---
+++ HEART_ATTACK_PREDICTION_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HeartAttackPredictionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HEART_ATTACK_PREDICTION_app'
